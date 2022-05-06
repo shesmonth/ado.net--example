fb5990e639cfc18756264bd657d398bcf57ac461
Your Name <you@example.com>
2022-05-06 13:32:25 +0800

    使用DataSet及GridView显示分组信息

---
+++ 9.3-2/default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="_9._3_2__使用DataSet及GridView显示分组信息._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 9.3-2/default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace _9._3_2__使用DataSet及GridView显示分组信息
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) //首次加载时调用
+            {
+                string connStr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    string sql = "select * from ContactGroup";
+                    SqlDataAdapter da = new SqlDataAdapter(sql,conn);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    //指定控件的数据源
+                    GridView1.DataSource = ds.Tables[0];
+                    //绑定数据
+                    GridView1.DataBind();
+                }
+            }
+        }
+    }
+}
