e990a977b3b332957fddf3d2139f633ee0e95a54
Your Name <you@example.com>
2022-05-06 13:34:04 +0800

    调用存储过程实现联系人分组录入

---
+++ 9.3-5/show.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="show.aspx.cs" Inherits="_9._3_5__调用存储过程实现联系人分组录入.show" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 143px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">分组名称：</td>
+                    <td>
+                        <asp:TextBox ID="txtGroupName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">备注：</td>
+                    <td>
+                        <asp:TextBox ID="txtGroupMemo" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="录入" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button2" runat="server" Text="取消" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 9.3-5/show.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _9._3_5__调用存储过程实现联系人分组录入
+{
+    public partial class show : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string groupName = txtGroupName.Text.Trim();
+            string memo = txtGroupMemo.Text.Trim();
+            string connString =ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string sql = "InsertContactGroup"; //存储过程名称
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //指定SqlCommand对象的CommandType类型为StoredProcedure
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@GroupName", groupName);
+                cmd.Parameters.AddWithValue("@Memo", memo);
+                conn.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n != 1)
+                {
+                    Response.Write("添加分组失败！");
+                }
+                else
+                {
+                    Response.Write("添加分组成功！");
+                }
+            }
+        }
+    }
+}
