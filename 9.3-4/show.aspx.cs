2b257d902e1a1411074a07f9468720692ae06f56
Your Name <you@example.com>
2022-05-06 13:33:40 +0800

    使用参数化sql语句实现联系人分组录入

---
+++ 9.3-4/show.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _9._3_4__使用参数化sql语句实现联系人分组录入
+{
+    public partial class show : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //获取文本框信息
+            string strGroupName = txtGroupName.Text;
+            string strGroupMemo = txtGroupMemo.Text;
+
+            //获取数据库连接字符串
+            string connStr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                //sql语句
+                string insertSql = "insert into ContactGroup values(@GroupName,@Memo)";
+                SqlCommand cmd = new SqlCommand(insertSql, conn);
+
+                //为参数赋值
+                cmd.Parameters.AddWithValue("@GroupName", strGroupName);
+                cmd.Parameters.AddWithValue("@Memo", strGroupMemo);
+
+                //执行sql语句
+                conn.Open();
+
+                int n = cmd.ExecuteNonQuery(); //执行并返回收影响的行数
+
+                if (n!=1)
+                {
+                    Response.Write("添加分组失败！");
+                }
+                else
+                {
+                    Response.Write("添加分组成功！");
+                }
+            }
+
+        }
+    }
+}
---
+++ 9.3-4/show.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace _9._3_4__使用参数化sql语句实现联系人分组录入 {
+    
+    
+    public partial class show {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// txtGroupName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtGroupName;
+        
+        /// <summary>
+        /// txtGroupMemo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtGroupMemo;
+        
+        /// <summary>
+        /// Button1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Button2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+    }
+}
